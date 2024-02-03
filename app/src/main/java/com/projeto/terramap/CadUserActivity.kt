21eb4d7c16f58062baa5ef93dc58bb638ea2dd3e
rafael <rafa.ro195@gmail.com>
2024-02-02 22:10:39 -0300

    Intent e Camera

--- app/src/main/java/com/projeto/terramap/CadUserActivity.kt
+++ app/src/main/java/com/projeto/terramap/CadUserActivity.kt
@@ -1,8 +1,15 @@
 package com.projeto.terramap
 
+import android.app.Notification.Action
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.provider.MediaStore
+import android.graphics.Bitmap
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 //import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 //import androidx.navigation.findNavController
@@ -12,6 +19,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import com.projeto.terramap.databinding.ActivityCadUserBinding
 
 class CadUserActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 100
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityCadUserBinding
@@ -22,27 +33,49 @@ class CadUserActivity : AppCompatActivity() {
         binding = ActivityCadUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //setSupportActionBar(binding.toolbar)
-
         // Move the setOnClickListener after inflating the layout
         binding.btCadLogin.setOnClickListener {
             IrparaTelaLogin()
         }
 
+        //setSupportActionBar(binding.toolbar)
+
+        imageView = findViewById(R.id.fotoUser)
+        button = findViewById(R.id.buttonFoto)
+
+        button.setOnClickListener() {
+            val tirarfotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(tirarfotoIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Error: " + e.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
         //val navController = findNavController(R.id.nav_host_fragment_content_cad_user)
         //appBarConfiguration = AppBarConfiguration(navController.graph)
         //setupActionBarWithNavController(navController, appBarConfiguration)
-    }
 
     override fun onStart() {
         super.onStart()
         val textView = findViewById<TextView>(R.id.textView)
-        textView.text = "Bem-vindo à tela de cadastro de usuário!"
+        textView.text = "cadastro de usuário"
     }
         private fun IrparaTelaLogin() {
             val LoginActivity = Intent(this, LoginActivity::class.java)
             startActivity(LoginActivity)
         }
+}
 
 
     //override fun onSupportNavigateUp(): Boolean {
@@ -50,4 +83,3 @@ class CadUserActivity : AppCompatActivity() {
     //    return navController.navigateUp(appBarConfiguration)
     //            || super.onSupportNavigateUp()
     //}
-}
