aaf53a0fe56a5cdca2704c268253fdfcd5e79970
rafael <rafa.ro195@gmail.com>
2024-02-08 21:42:51 -0300

    Notificação

--- app/src/main/java/com/projeto/terramap/MainActivity.kt
+++ app/src/main/java/com/projeto/terramap/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.projeto.terramap
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
-import android.view.View
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-//import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-//import androidx.navigation.findNavController
-//import androidx.navigation.NavController
-//import androidx.navigation.findNavController
-//import androidx.navigation.fragment.NavHostFragment
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.projeto.terramap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val CHANNEL_ID = "channelID"
+    private val CHANNEL_NAME = "channelName"
+    private val NOTIFICATION_ID = 0
+    private lateinit var notificationManager: NotificationManagerCompat
     private lateinit var binding: ActivityMainBinding
     //private lateinit var navController: NavController
 
@@ -22,16 +30,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        createNotificationChannel()
 
-        // Initialize NavController after layout inflation
-        //val navHostFragment =
-        //    supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        //navController = navHostFragment.navController
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Seja bem vindo")
+            .setContentText("Venha conhecer nossos serviços! Temos o objetivo otimizar tarefas e fornecer uma interface" +
+                    "intuitiva para que o usuário possa realizar a manipulação de dados com facilidade, a fim de contribuir" +
+                    "com todos os pesquisadores da área e proporcionar eficiência ao utilizá-lo")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Venha conhecer nossos serviços! Temos o objetivo otimizar tarefas e fornecer uma interface" +
+                            "intuitiva para que o usuário possa realizar a manipulação de dados com facilidade, a fim de contribuir" +
+                            "com todos os pesquisadores da área e proporcionar eficiência ao utilizá-lo")
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-        //binding.nextLogin.setOnClickListener {
-        //    IrparaTelaLogin()
-        //}
+        notificationManager = NotificationManagerCompat.from(this)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Solicitar permissão para notificação
+        } else {
+            notificationManager.notify(NOTIFICATION_ID, builder.build())
+        }
     }
+
     fun Next(view: View) {
         IrparaTelaLogin()
     }
@@ -51,5 +79,21 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channelName)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+
+
     }
 }
